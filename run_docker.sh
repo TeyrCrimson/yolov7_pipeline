7ff6cd7d290b57b7cebc41d591bd5335a674144e
tehwenyi <teh.wenyi22@gmail.com>
2023-05-29 17:25:26 +0800

    add run_docker.sh file

--- run_docker.sh
+++ run_docker.sh
@@ -1,9 +1,21 @@
 WORKSPACE=/media/data/yolov7
 DATA=/media/data/datasets
 
+xhost +local:docker
 docker run -it --rm \
+	--platform=linux/amd64 \
 	--gpus all \
-    -w $WORKSPACE \
+	--device /dev/video1 \
+	--device /dev/video2 \
+	--device /dev/video3 \
+	--device /dev/video4 \
+	--device /dev/video5 \
+  -w $WORKSPACE \
 	-v $WORKSPACE:$WORKSPACE \
 	-v $DATA:$DATA \
-	yolov7
+	--ipc=host \
+  -v $HOME/.Xauthority:/root/.Xauthority:rw \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -e DISPLAY=unix$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+	yolov7-inference-rs
