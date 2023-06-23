df5f3959622a6bed1c89c9ae765fdaa71cd7e2cf
tehwenyi <teh.wenyi22@gmail.com>
2023-06-23 18:09:18 +0800

    update bash script to run xavier/inference_rs.py

--- xavier/run_inference_rs.sh
+++ xavier/run_inference_rs.sh
@@ -1,6 +1,23 @@
-python3 inference_rs.py \
-  -w /mnt/rootfs/realsense/yolov7/weights/best.pt \
-  -c /mnt/rootfs/realsense/yolov7/cfg/deploy/yolov7-tiny.yaml \
-  --width 640 \
-  --height 480 \
-  --fps 30
+WEIGHTS='/mnt/rootfs/realsense/yolov7/weights/best.pt'
+CFG='/mnt/rootfs/realsense/yolov7/cfg/deploy/yolov7-tiny.yaml'
+INFERENCE_FOLDER='/mnt/rootfs/realsense/output/inference_videos'
+RAW_VIDEO_FOLDER='/mnt/rootfs/realsense/output/raw_videos'
+
+WIDTH=1280
+HEIGHT=720
+FPS=30
+
+python inference_rs.py \
+  -w $WEIGHTS \
+  -c $CFG \
+  --bgr \
+  --gpu_device 0 \
+  --model_image_size 1280 \
+  --max_batch_size 64 \
+  --conf_thresh 0.25 \
+  --nms_thresh 0.45 \
+  --inference-folder $INFERENCE_FOLDER \
+  --raw-video-folder $RAW_VIDEO_FOLDER \
+  --width $WIDTH \
+  --height $HEIGHT \
+  --fps $FPS
