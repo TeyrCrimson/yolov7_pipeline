eccebf810952c1a003c95d8d9478eb44e27d10c7
tehwenyi <teh.wenyi22@gmail.com>
2023-06-23 17:57:09 +0800

    implement parameter customization for YOLOv7 using argparse

--- xavier/inference_rs.py
+++ xavier/inference_rs.py
@@ -32,29 +32,29 @@ def yuyv_to_bgr(color_frame):
 
   return bgr
 
-def main(weights, cfg, inference_folder, raw_video_folder, fps, vid_width, vid_height):
+def main(args):
   # Check if weights file exists
-  if not weights.is_file():
-    raise FileNotFoundError(f"Unable to find weights file: {weights}")
+  if not args.weights.is_file():
+    raise FileNotFoundError(f"Unable to find weights file: {args.weights}")
 
   # Check if cfg file exists
-  if not cfg.is_file():
-    raise FileNotFoundError(f"Unable to find cfg file: {cfg}")
+  if not args.cfg.is_file():
+    raise FileNotFoundError(f"Unable to find cfg file: {args.cfg}")
 
   # Initialise YOLOv7 inference model
-  # TODO: allow editing of these parameters outside of this code
   yolov7 = YOLOv7(
-    weights=weights,
-    cfg=cfg,
-    bgr=True,
-    gpu_device=0,
-    model_image_size=640,
-    max_batch_size=64,
-    half=True,
-    same_size=True,
-    conf_thresh=0.25,
-    trace=False,
-    cudnn_benchmark=False,
+    weights=args.weights,
+    cfg=args.cfg,
+    bgr=args.bgr,
+    gpu_device=args.gpu_device,
+    model_image_size=args.model_image_size,
+    max_batch_size=args.max_batch_size,
+    half=args.half,
+    same_size=args.same_size,
+    conf_thresh=args.conf_thresh,
+    nms_thresh=args.nms_thresh,
+    trace=args.trace,
+    cudnn_benchmark=args.cudnn_benchmark,
   )
 
   # Configure depth and color streams
@@ -76,21 +76,21 @@ def main(weights, cfg, inference_folder, raw_video_folder, fps, vid_width, vid_h
     print("The demo requires a Depth camera with Color sensor")
     exit(0)
 
-  print(f"Stream configs: {vid_width}x{vid_height} with {fps} fps")
-  config.enable_stream(rs.stream.color, vid_width, vid_height, rs.format.yuyv, fps)
+  print(f"Stream configs: {args.width}x{args.height} with {args.fps} fps")
+  config.enable_stream(rs.stream.color, args.width, args.height, rs.format.yuyv, args.fps)
 
-  if inference_folder:
+  if args.inference_folder:
     try:
-      inf_video_writer = create_output_video_writer('inference', inference_folder, fps, vid_width, vid_height)
+      inf_video_writer = create_output_video_writer('inference', args.inference_folder, args.fps, args.width, args.height)
     except Exception as e:
-      print(f"ERROR: Failed to create video file n the specified inference folder ({inference_folder}):\n{e}")
+      print(f"ERROR: Failed to create video file n the specified inference folder ({args.inference_folder}):\n{e}")
       exit(0)
     
-  if raw_video_folder:
+  if args.raw_video_folder:
     try:
-      raw_video_writer = create_output_video_writer('raw', raw_video_folder, fps, vid_width, vid_height)
+      raw_video_writer = create_output_video_writer('raw', args.raw_video_folder, args.fps, args.width, args.height)
     except Exception as e:
-      print(f"ERROR: Failed to create video file n the specified inference folder ({inference_folder}):\n{e}")
+      print(f"ERROR: Failed to create video file n the specified raw video folder ({args.raw_video_folder}):\n{e}")
       exit(0)
 
   # Start streaming
@@ -150,13 +150,26 @@ def main(weights, cfg, inference_folder, raw_video_folder, fps, vid_width, vid_h
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument('-w', '--weights', type=Path, required=True, help='path to YOLOv7 weights')
-  parser.add_argument('-c', '--cfg', type=Path, required=True, help='path to YOLOv7 config file')
-  parser.add_argument('--inference-folder', type=Path, default=None, help='folder to save inference videos; videos named by start recording time')
-  parser.add_argument('--raw-video-folder', type=Path, default=None, help='folder to save raw videos (i.e. no annotations); videos named by start recording time')
-  parser.add_argument('--width', type=int, default=1280, help='desired width of video')
-  parser.add_argument('--height', type=int, default=720, help='desired height of video')
-  parser.add_argument('--fps', type=int, default=30, help='desired fps of video')
-  opt = parser.parse_args()
+  # YOLOv7 parameters
+  parser.add_argument('-w', '--weights', type=Path, required=True, help='Path to YOLOv7 weights')
+  parser.add_argument('-c', '--cfg', type=Path, required=True, help='Path to YOLOv7 config file')
+  parser.add_argument('--bgr', action='store_true', help='Set to use BGR color space (default: BGR)')
+  parser.add_argument('--gpu_device', type=int, default=0, help='GPU device index (default: 0)')
+  parser.add_argument('--model_image_size', type=int, default=1280, help='Input image size for the model (default: 640)')
+  parser.add_argument('--max_batch_size', type=int, default=64, help='Maximum batch size for inference (default: 64)')
+  parser.add_argument('--half', action='store_true', help='Enable half precision inference')
+  parser.add_argument('--same_size', action='store_true', help='Use the same size for all images during inference')
+  parser.add_argument('--conf_thresh', type=float, default=0.25, help='Confidence threshold for object detection (default: 0.25)')
+  parser.add_argument('--nms_thresh', type=float, default=0.45, help='Non-maximum suppression threshold (default: 0.45)')
+  parser.add_argument('--trace', action='store_true', help='Enable tracing for better performance')
+  parser.add_argument('--cudnn_benchmark', action='store_true', help='Enable cuDNN benchmark mode')
+  
+  # Video parameters
+  parser.add_argument('--inference-folder', type=Path, default=None, help='Folder to save inference videos; videos named by start recording time')
+  parser.add_argument('--raw-video-folder', type=Path, default=None, help='Folder to save raw videos (i.e. no annotations); videos named by start recording time')
+  parser.add_argument('--width', type=int, default=1280, help='Desired width of video')
+  parser.add_argument('--height', type=int, default=720, help='Desired height of video')
+  parser.add_argument('--fps', type=int, default=30, help='Desired fps of video')
+  args = parser.parse_args()
 
-  main(opt.weights, opt.cfg, opt.inference_folder, opt.raw_video_folder, opt.fps, opt.width, opt.height)
+  main(args)
